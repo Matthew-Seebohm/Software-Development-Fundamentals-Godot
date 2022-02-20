28e3ef0be221194392e96bc2a99260a83bf153d0
abhishekwick <7064470@schoolsnet.act.edu.au>
2022-02-21 10:24:19 +1100

    added the timer feature and fixed the boundries

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,10 @@
 extends KinematicBody2D
 
-var movement_speed = 200
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var movement_speed = 400
 
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
 	set_physics_process(true)
 
@@ -15,3 +13,7 @@ func _physics_process(delta):
 		move_and_collide(Vector2(-movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_right"):
 		move_and_collide(Vector2(movement_speed * delta, 0))
+	if position.x <= 10:
+		move_and_collide(Vector2(movement_speed * delta, 0))
+	if position.x >= 1250:
+		move_and_collide(Vector2(-movement_speed * delta, 0))
